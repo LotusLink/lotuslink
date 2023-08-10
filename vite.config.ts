370eb59595a163742b3ae802b03b1045df820e43
Patrick Skinner <78289253+PSkinnerTech@users.noreply.github.com>
2023-08-10 10:14:15 -0500

    Initial commit from create-permaweb-app

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+// @ts-ignore
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      process: 'process/browser',
+      util: 'util',
+    },
+  },
+  base: '',
+  plugins: [react()],
+});
